5626f4c0ed7c75bbc5274f6ae63cbcd85f7cbf85
yunxuejian666 <49329634+yunxuejian666@users.noreply.github.com>
2021-02-02 20:59:23 +0800

    Update Hello.java

--- meinian_common/src/main/java/com/atguigu/Hello.java
+++ meinian_common/src/main/java/com/atguigu/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("hello hotfix");
         System.out.println("hello hotfix1");
+        System.out.println("1")
         System.out.println("master");
         System.out.println("hotfix");
     }
